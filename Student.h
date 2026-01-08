9b066062269a26ac5f0b7273ea6dd7d02ffda724
divya-garg <dgarg17306@gmail.com>
2026-01-08 19:58:44 +0530

    Add Student class declaration

---
+++ Student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student {
+private:
+int rollNo;
+char name[50];
+int age;
+char course[30];
+public:
+void input();
+void display();
+int getRollNo();
+};
+
+#endif
