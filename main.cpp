f07e842d7dbbbcb2ae7eecfb0718363908447202
divya-garg <dgarg17306@gmail.com>
2026-01-08 20:07:14 +0530

    Implement add student with binary file storage

--- main.cpp
+++ main.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
+#include <fstream>
 #include "Student.h"
+
 using namespace std;
 
-int main() {
-int choice;
-do {
-cout << "\nStudent Record Management System\n";
-cout << "1. Add Student\n";
-cout << "2. View Students\n";
-cout << "3. Search Student\n";
-cout << "4. Modify Student\n";
-cout << "5. Delete Student\n";
-cout << "6. Exit\n";
-cout << "Enter your choice: ";
-cin >> choice;
+void addStudent() {
+    Student s;
+    fstream file("students.dat", ios::binary | ios::in | ios::out | ios::app);
 
-switch (choice) {
-case 1:
-cout << "Add Student selected\n";
-break;
-case 2:
-cout << "View Students selected\n";
-break;
-case 3:
-cout << "Search Student selected\n";
-break;
-case 4:
-cout << "Modify Student selected\n";
-break;
-case 5:
-cout << "Delete Student selected\n";
-break;
-case 6:
-cout << "Exiting program...\n";
-break;
-default:
-cout << "Invalid choice. Try again.\n";
-}} while (choice != 6);
- return 0;
+    if (!file) {
+        cout << "Error opening file.\n";
+        return;
+    }
+
+    s.input();
+    file.write((char*)&s, sizeof(s));
+    file.close();
+
+    cout << "Student record added successfully.\n";
+}
+
+int main() {
+    int choice;
+
+    do {
+        cout << "\n--- Student Record Management System ---\n";
+        cout << "1. Add Student\n";
+        cout << "2. View Students\n";
+        cout << "3. Search Student\n";
+        cout << "4. Modify Student\n";
+        cout << "5. Delete Student\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addStudent();
+                break;
+            case 6:
+                cout << "Exiting program...\n";
+                break;
+            default:
+                cout << "Option not implemented yet.\n";
+        }
+
+    } while (choice != 6);
+
+    return 0;
 }
